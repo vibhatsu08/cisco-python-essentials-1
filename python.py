b4491d3ee086b124dd9ac7f7c86ae7597b0ff10f
vibhatsu08 <mistryvedant17@gmail.com>
2022-12-19 02:11:38 +0530

    python program to calculate the BMI of a person based on their weight and height.

--- python.py
+++ python.py
@@ -1,14 +1,7 @@
-# python program to print all the unique numbers in a given list
-numbers = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
-unique_numbers = []
+# python program to calculate the BMI of a person based on their weight and height.
+# the function takes in two parameters, height and weight
+def calculateBMI (height, weight):
+    # returns the weight/height**2
+    return ((weight)/(height)**2)
 
-for number in numbers :
-    if number not in unique_numbers :
-        unique_numbers.append(number)
-        
-print(unique_numbers)
-
-# this is how the program works
-# the list is provided with the input numbers and duplicates
-# first step is to loop through the original list, and the later steps are to compare the value present in the unique list to the original list to check for any duplicates, and once that's done, the next step is to append the non repeating element in the unique list.
-# finally, print the output of the program.
+print(calculateBMI(1.72, 74))
