82a615a1a419e96abd103d4aab6460f4eb2c6c21
vibhatsu08 <mistryvedant17@gmail.com>
2022-12-19 02:44:36 +0530

    python program to print the factorial of a given number.

--- python.py
+++ python.py
@@ -1,8 +1,8 @@
 # python program to print the factorial of a given number.
 def printFactorial (number) :
     product = 1
-    for digit in range (2, number+1):
+    for digit in range (1, number+1):
         product *= digit
     return product
     
-print (printFactorial(5))
+print (printFactorial(0))
