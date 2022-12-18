d6d6999efc25a28331632919a08f7bbac8aadeaa
vibhatsu08 <mistryvedant17@gmail.com>
2022-12-19 03:57:37 +0530

    python program to glue two dictionaries together and create a new dictionary with that value.

--- python.py
+++ python.py
@@ -1,10 +1,15 @@
-# python program to print the fibonacci sequence of numbers using recursion.
-def printFibonacci(number) :
-    if (number <= 0) :
-        return 0
-    elif (number == 1) :
-        return 1
-    else :
-        return printFibonacci(number-1) + printFibonacci(number-2)
+# python program to "glue" two dictionaries together and create a new dictionary with that value.
+dict1 = {
+    "batman": "robert pattinson",
+    "black adam": "dwayne johnson"
+}
+dict2 = {
+    "ironman": "robert downey junior",
+    "spiderman": "tom holland"
+}
+dict3 = {}
 
-print(printFibonacci(10))
+for item in (dict1, dict2) :
+    dict3.update(item)    
+    
+print(dict3)
