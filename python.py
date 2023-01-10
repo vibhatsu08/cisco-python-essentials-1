6269efb5d2b6137f8a39c0192dc573565131a5a2
vibhatsu08 <mistryvedant17@gmail.com>
2023-01-11 02:26:53 +0530

    Using the stack, the procedural approach

--- python.py
+++ python.py
@@ -1,5 +1,8 @@
-# the first class
-class closet :
-   pass
-# the first object
-book = closet()
+# Using the stack, the procedural approach
+stack = []
+def push (value) :
+   stack.append(value)
+def pop (value) :
+   value = stack[-1]
+   del value
+   return value
