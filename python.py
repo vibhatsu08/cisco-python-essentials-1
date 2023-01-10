0713c0caed73af2f6fcfc70ae7fa0f76898bcd49
vibhatsu08 <mistryvedant17@gmail.com>
2023-01-11 01:58:46 +0530

    My first class and my first object.

--- python.py
+++ python.py
@@ -1,15 +1,5 @@
-# python program to "glue" two dictionaries together and create a new dictionary with that value.
-dict1 = {
-    "batman": "robert pattinson",
-    "black adam": "dwayne johnson"
-}
-dict2 = {
-    "ironman": "robert downey junior",
-    "spiderman": "tom holland"
-}
-dict3 = {}
-
-for item in (dict1, dict2) :
-    dict3.update(item)    
-    
-print(dict3)
+# the first class
+class closet :
+   pass
+# the first object
+book = closet()
